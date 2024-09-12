84b634fff03d5b6390b60d50ba77b84ea98623ef
Lassi Mäntylä <lassi.mantyla@gmail.com>
2024-09-12 23:52:46 +0300

    init

---
+++ change.php
@@ -0,0 +1,44 @@
+<?php
+/** @psalm-suppress PossiblyUndefinedArrayOffset */
+if ($_SERVER["REQUEST_METHOD"]==="OPTIONS") {
+	exit;
+}
+
+$conn = @pg_connect("dbname=pakastin");
+if (!$conn || !array_key_exists("key",$_GET)) {
+	header('HTTP/1.0 403 Forbidden');
+	exit;
+}
+/** @psalm-suppress MixedArgument */
+$hash = base64_encode(hash("sha256", htmlspecialchars($_GET["key"]), true));
+/** @psalm-suppress MixedAssignment */
+$hash1 = pg_escape_literal($hash);
+$access = pg_query($conn, "select * from access where \"key\"=$hash1;");
+if (!(pg_fetch_row($access)[0])) {
+	header('HTTP/1.0 403 Forbidden');
+	exit;
+}
+/** @psalm-suppress MixedAssignment */
+$owner = pg_escape_literal($_GET["name"]);
+/** @psalm-suppress MixedAssignment */
+$item = pg_escape_literal($_GET["item"]);
+if (!is_numeric($_GET["amount"])) {
+	exit;
+}
+$existing = pg_query($conn, "select * from manifest where lower(owner)=lower($owner) and lower(item)=lower($item);");
+/** @psalm-suppress MixedAssignment */
+$amount = pg_escape_literal($_GET["amount"]);
+if ($_GET["amount"]==0) {
+	pg_query($conn, "delete from manifest where lower(owner)=lower($owner) and lower(item)=lower($item);");
+}
+elseif (!$existing || !pg_fetch_row($existing)[0]) {
+	pg_query($conn, "insert into manifest values ($owner,$item,$amount);");
+}
+else {
+	/** @psalm-suppress MixedAssignment */
+	$stuff = pg_fetch_row($existing)[0];
+	pg_query($conn, "update manifest set amount=$amount where lower(owner)=lower($owner) and lower(item)=lower($item);");
+	pg_query($conn, "update manifest set owner=$owner where lower(owner)=lower($owner);");
+}
+include "index.php";
+?>
---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+	<link rel="stylesheet" href="/styles.css">
+</html>
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header('Access-Control-Allow-Methods: GET');
+$conn = @pg_connect("dbname=pakastin");
+if (!$conn) {
+	echo "the database is fucked";
+	echo (pg_last_error());
+	exit;
+}
+$owners = @pg_query($conn, "select distinct on (owner) owner from manifest order by owner;");
+if (!$owners) {
+	echo "the query is fucked";
+	echo (pg_last_error($conn));
+  exit;
+}
+echo "<h3>Pakastimen sisällysluettelo</h3>";
+echo "<ul>";
+while ($owner = pg_fetch_row($owners)) {
+	echo "<li><p class='owner'>$owner[0]</p><ul>";
+	/** @psalm-suppress MixedAssignment */
+	$ownerid = pg_escape_literal($owner[0]);
+	$belongings = @pg_query($conn, "select amount,item from manifest where owner=$ownerid order by amount desc;");
+	while ($stuff = pg_fetch_row($belongings)) {
+		echo "<li>$stuff[0]x $stuff[1]</li>";
+	}
+	echo "</ul></li>";
+}
+echo "</ul>";
+?>
